c0a5c87ea956f7f434a12bd11b574d34c71822ca
Lena19 <lena_naumova_19@mail.ru>
2016-11-15 08:26:18 +0200

    Full

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -10,6 +10,18 @@ namespace Facade
     {
         static void Main(string[] args)
         {
+            Amplifier amp = new Amplifier();
+            DvdPlayer dvd = new DvdPlayer();
+            PopcornPopper popper = new PopcornPopper();
+            Projector projector = new Projector();
+            Screen screen = new Screen();
+            TheaterLights lights = new TheaterLights();
+            Tuner tuner = new Tuner();
+            CdPlayer cd = new CdPlayer();
+
+            HomeTheaterFacade facade = new HomeTheaterFacade(amp, tuner, dvd, cd, projector, screen, lights, popper);
+            facade.watchMovie("The Lord of the Rings");
+            facade.endMovie();
         }
     }
 }
